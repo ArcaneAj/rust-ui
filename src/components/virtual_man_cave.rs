dd993ecbd7b6d97daa6075d7aa2fb3497369621e
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2023-02-13 22:56:30 +1300

    Split man cave up into private components and added buttons to swap between them

--- src/components/virtual_man_cave.rs
+++ src/components/virtual_man_cave.rs
@@ -6,15 +6,107 @@ pub struct VirtualManCaveComponentProps {
 
 #[function_component(VirtualManCave)]
 pub fn virtual_man_cave_component(VirtualManCaveComponentProps {}: &VirtualManCaveComponentProps) -> Html {
+    let counter = use_state(|| 0);
+    
+    let on_click_next = {
+        let counter = counter.clone();
+        move |_| {
+            let value = (*counter + 1) % WIDGET_COUNT;
+            counter.set(value);
+        }
+    };
+    
+    let on_click_back = {
+        let counter = counter.clone();
+        move |_| {
+            let value = (*counter + WIDGET_COUNT - 1) % WIDGET_COUNT;
+            counter.set(value);
+        }
+    };
+
     html! { 
-        <h1 class={classes!("mountain-text")}>
-        <title>{"Certified old school time wasters:"}</title>
-        <p>{ "Vsauce" }</p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/jHbyQ_AQP8c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=true></iframe>
-        <p> { "Cool math games" } </p>
-        <iframe src="https://www.miniplay.com/embed/bloxorz" width="600" height="400" frameborder="0" allowfullscreen=true></iframe>
-        // todo: embed this properly without ruining the webpage layout
-        // <p> {"park the taxi game"} </p>
-        // <iframe src="https://1000webgames.com/games/parkthetaxi/html5/" frameborder="0" width="800" height="450" scrolling="no" allowfullscreen=true></iframe>
-        </h1> }
+        <div class={classes!("man-cave")}>
+            <div>
+                <h1 style="display: inline-block;">{"Certified old school time wasters:"}</h1>
+                <div style="float: right;">
+                    <button onclick={on_click_back}>{ "Back" }</button>
+                    <button onclick={on_click_next}>{ "Next" }</button>
+                </div>
+            </div>
+            { switch(get_widget_by_index(*counter)) }
+        </div>
+         }
+}
+
+fn get_widget_by_index(index: usize) -> Widget {
+    match index {
+        0 => Widget::YouTube,
+        1 => Widget::Bloxorz,
+        2 => Widget::Taxi,
+        _ => Widget::YouTube, // Fallback should not happen if done correctly, can be done safer I'm sure
+    }
+}
+
+const WIDGET_COUNT: usize = 3;
+
+#[derive(Clone, PartialEq)]
+enum Widget {
+    YouTube,
+    Bloxorz,
+    Taxi,
+}
+
+fn switch(routes: Widget) -> Html {
+    match routes {
+        Widget::YouTube => html! { <YouTube title="Vsauce" link="https://www.YouTube.com/embed/jHbyQ_AQP8c"/> },
+        Widget::Bloxorz => html! { <Bloxorz/> },
+        Widget::Taxi => html! { <Taxi/> },
+    }
+}
+
+#[derive(PartialEq, Properties)]
+struct YouTubeComponentProps {
+    pub title: AttrValue,
+    pub link: AttrValue,
+}
+
+#[function_component(YouTube)]
+fn youtube_component(YouTubeComponentProps { title, link }: &YouTubeComponentProps) -> Html {
+
+    html! { 
+        <div id="YouTube">
+            <p>{ title }</p>
+            <iframe src={ link } width="560" height="315" frameborder="0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=true></iframe>
+        </div>
+         }
+}
+
+#[derive(PartialEq, Properties)]
+struct BloxorzComponentProps {
+}
+
+#[function_component(Bloxorz)]
+fn bloxorz_component(BloxorzComponentProps {}: &BloxorzComponentProps) -> Html {
+
+    html! { 
+        <div id="bloxorz">
+            <p> { "Cool math games" } </p>
+            <iframe src="https://www.miniplay.com/embed/bloxorz" width="560" height="315" frameborder="0" allowfullscreen=true></iframe>
+        </div>
+         }
+}
+
+#[derive(PartialEq, Properties)]
+struct TaxiComponentProps {
+}
+
+#[function_component(Taxi)]
+fn taxi_component(TaxiComponentProps {}: &TaxiComponentProps) -> Html {
+
+    html! { 
+        <div id="bloxorz">
+        <p> { "Park the taxi game" } </p>
+        <iframe src="https://1000webgames.com/games/parkthetaxi/html5/" width="560" height="315" frameborder="0" scrolling="no" allowfullscreen=true></iframe>
+        </div>
+         }
 }
